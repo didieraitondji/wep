73fe1337061b33cbe952bfddca58d1f95c79e6d4
Didier Tolome <didier.tolomeaitondji@gmail.com>
2024-06-30 23:07:31 +0200

    Delete frontend directory

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import Menu from "./composants/Menu";
-import Accueil from "./vues/AccueilPage";
-
-function App() {
-  return <Accueil />;
-}
-
-export default App;
--- frontend/src/composants/HeadPlace.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function HeadPlace() {
-  return <div className="h-[53px]"></div>;
-}
