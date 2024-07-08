3a7a26c7372be57f9cc5fe487c5d2606e3384ae1
didos <didier.tolomeaitondji@gmail.com>
2024-07-08 20:37:12 +0100

    passage au nouveau design

--- src/composants/Bande.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Bande({ id }) {
-  return <div id={id} className="w-[100%] h-[100px] bg-white"></div>;
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,14 @@
 import React from "react";
 import 'typeface-poppins';
 import ReactDOM from "react-dom/client";
-import "./assets/css/index.css";
+import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Accueil from "./vues/AccueilPage.jsx";
-import ConnexionPage from "./vues/ConnexionPage.jsx";
-import ContactPage from "./vues/ContactPage.jsx";
-import Apropos from "./vues/Apropos.jsx";
-
-// création du composant
 
 const Root = () => {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Accueil />} />
-        <Route exact path="/connexion/" element={<ConnexionPage />} />
-        <Route exact path="/contact/" element={<ContactPage />} />
-        <Route exact path="/apropos/" element={<Apropos />} />
+        <Route exact path="/" element={} />
       </Routes>
     </Router>
   );
