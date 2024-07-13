546e85541bbb7e620310833fd0a42e04cac8092e
didos <didier.tolomeaitondji@gmail.com>
2024-07-13 22:08:06 +0100

    commit du M Majuscule

---
+++ src/Main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import 'typeface-poppins';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Destination from "./Destination";
+import ConnexionEtudiant from "./etudiant/Connexion";
+
+
+const Root = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Destination />} />
+                <Route exact path="/etudiant-connexion/" element={<ConnexionEtudiant />} />
+            </Routes>
+        </Router>
+    );
+};
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<Root />);
