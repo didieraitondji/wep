0ccbfd9ad122a80a6b0fb286f2e26dd4b282150f
didos <didier.tolomeaitondji@gmail.com>
2024-07-14 11:49:29 +0100

    v2 14/07/2024

--- src/composants/destination/Fonctionnalites.jsx
+++ src/composants/destination/Fonctionnalites.jsx
@@ -257,8 +257,79 @@ export default function Fonctionnalites() {
                     </div>
 
                     <div className={`w-[100%] lg:w-[50%] mb-1 ${!isEnd && 'hidden'}`}>
-                        <div className='w-[95%] px-4 py-6 mb-5 mx-auto border border-c4 rounded-md text-justify'>
+                        <div className='w-[95%] px-4 py-6 mb-5 mx-auto border border-c4 rounded-md text-justify text-white'>
                             <h1 className='font-bold text-white mb-6'>Pour les administratifs !</h1>
+
+                            <p className='text-c1 font-bold'>
+                                Les administratifs sur WeP auront la possibilité de :
+                            </p><br />
+                            <div className=' leading-10'>
+                                <ul>
+                                    <li className='grid grid-cols-[45px_auto] items-center'>
+                                        <span className='pl-3 text-left text-c1'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard2-check-fill" viewBox="0 0 16 16">
+                                                <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5" />
+                                                <path d="M4.085 1H3.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1h-.585q.084.236.085.5V2a1.5 1.5 0 0 1-1.5 1.5h-5A1.5 1.5 0 0 1 4 2v-.5q.001-.264.085-.5m6.769 6.854-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708.708" />
+                                            </svg>
+                                        </span>
+                                        <span>
+                                            Gérer les utilisateurs :
+                                        </span>
+                                    </li>
+
+                                    <ul className='pl-5'>
+                                        <li className='grid grid-cols-[45px_auto] items-center'>
+                                            <span className='pl-3 text-left text-c1'>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard2-check-fill" viewBox="0 0 16 16">
+                                                    <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5" />
+                                                    <path d="M4.085 1H3.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1h-.585q.084.236.085.5V2a1.5 1.5 0 0 1-1.5 1.5h-5A1.5 1.5 0 0 1 4 2v-.5q.001-.264.085-.5m6.769 6.854-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708.708" />
+                                                </svg>
+                                            </span>
+                                            <span>
+                                                Créer Utilisateur
+                                            </span>
+                                        </li>
+
+                                        <li className='grid grid-cols-[45px_auto] items-center'>
+                                            <span className='pl-3 text-left text-c1'>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard2-check-fill" viewBox="0 0 16 16">
+                                                    <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5" />
+                                                    <path d="M4.085 1H3.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1h-.585q.084.236.085.5V2a1.5 1.5 0 0 1-1.5 1.5h-5A1.5 1.5 0 0 1 4 2v-.5q.001-.264.085-.5m6.769 6.854-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708.708" />
+                                                </svg>
+                                            </span>
+                                            <span>
+                                                Mettre à jour Utilisateur
+                                            </span>
+                                        </li>
+
+                                        <li className='grid grid-cols-[45px_auto] items-center'>
+                                            <span className='pl-3 text-left text-c1'>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard2-check-fill" viewBox="0 0 16 16">
+                                                    <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5" />
+                                                    <path d="M4.085 1H3.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1h-.585q.084.236.085.5V2a1.5 1.5 0 0 1-1.5 1.5h-5A1.5 1.5 0 0 1 4 2v-.5q.001-.264.085-.5m6.769 6.854-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708.708" />
+                                                </svg>
+                                            </span>
+                                            <span>
+                                                Supprimer Utilisateur
+                                            </span>
+                                        </li>
+                                    </ul>
+                                    <li className='grid grid-cols-[45px_auto] items-center'>
+                                        <span className='pl-3 text-left text-c1'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard2-check-fill" viewBox="0 0 16 16">
+                                                <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5" />
+                                                <path d="M4.085 1H3.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1h-.585q.084.236.085.5V2a1.5 1.5 0 0 1-1.5 1.5h-5A1.5 1.5 0 0 1 4 2v-.5q.001-.264.085-.5m6.769 6.854-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708.708" />
+                                            </svg>
+                                        </span>
+                                        <span>
+                                            Ect.
+                                        </span>
+                                    </li>
+                                </ul> <br />
+                                <span>
+                                    <a href='/admin-connexion/' target='_blank' className='bg-c1 text-c3 px-3 py-1 rounded-full font-bold slide-up1 text-lg hover:bg-c4 hover:px-2 hover:text-c3'> Connexion ... </a>
+                                </span>
+                            </div>
                         </div>
                     </div>
 
